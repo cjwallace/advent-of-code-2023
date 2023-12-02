079e5e6c8517a868541fcc9fc44c7004a1f557d1
cjwallace <chris@bitcollider.io>
2023-12-02 13:28:26 +0000

    Solve day 01 part 02

--- day-01/solution.ts
+++ day-01/solution.ts
@@ -1,16 +1,71 @@
-import * as _ from "lodash";
+const lineToNumber = (
+  line: string,
+  lineToDigits: (line: string) => string[]
+): number => {
+  const digits = lineToDigits(line);
+  const first = digits[0];
+  const last = digits[digits.length - 1];
 
-const re = /\d/g;
+  return Number(first + last);
+};
 
-const extractNumber = (input: string): number =>
-  _.chain([...input.matchAll(re)])
-    .map(_.head)
-    .thru((arr: string[]) => _.join([_.first(arr), _.last(arr)], ""))
-    .thru(Number);
+// part 01
 
-export const part01 = (input: string): number =>
-  _.chain(input).split("\n").map(extractNumber).sum().value();
+const digitRegex = /\d/g;
+const numberLineToDigits = (line: string): string[] =>
+  [...line.matchAll(digitRegex)].map((x) => x[0]);
+
+export const part1 = (input: string): number =>
+  input
+    .split("\n")
+    .map((line: string) => lineToNumber(line, numberLineToDigits))
+    .reduce((a, b) => a + b, 0);
+
+// part 02
+
+const numbers = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+const numberWordRegex =
+  /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g;
+
+const wordToDigit = (word: string): string =>
+  word.length === 1 ? word : numbers.indexOf(word).toString();
+
+const wordLineToDigits = (line: string): string[] => {
+  const matches = line.matchAll(numberWordRegex);
+
+  const out = [];
+  for (const match of matches) {
+    if (match.index !== undefined) {
+      const numberName = line.slice(match.index, match.index + match[1].length);
+      const n = wordToDigit(numberName);
+      out.push(n);
+    }
+  }
+  return out;
+};
+
+export const part2 = (input: string): number =>
+  input
+    .split("\n")
+    .map((line: string) => lineToNumber(line, wordLineToDigits))
+    .reduce((a, b) => a + b, 0);
 
 // run
-const input = await Deno.readTextFile("./input.txt");
-console.log(part01(input));
+
+if (import.meta.main) {
+  const input = await Deno.readTextFile("./input.txt");
+  console.log(part01(input));
+  console.log(part02(input));
+}
--- day-01/test.ts
+++ day-01/test.ts
@@ -1,13 +1,26 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 
-import { part01 } from "./solution.ts";
+import { part1, part2 } from "./solution.ts";
 
-Deno.test("part-01", () => {
-  const input = `
+Deno.test("part-1", () => {
+  const input = `\
     1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
-    treb7uchet
+    treb7uchet\
   `;
-  assertEquals(part01(input), 142);
+  assertEquals(part1(input), 142);
+});
+
+Deno.test("part-2", () => {
+  const input = `\
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen\
+  `;
+  assertEquals(part2(input), 281);
 });
