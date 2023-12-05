c70ac0e92029f20dd17f5aace66f28afea4e03c3
cjwallace <chris@bitcollider.io>
2023-12-05 09:53:51 +0000

    Refactor tests to common inputs

--- day-02/test.ts
+++ day-02/test.ts
@@ -2,24 +2,18 @@ import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 
 import { part1, part2 } from "./solution.ts";
 
+const input = `\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\
+`;
+
 Deno.test("part-1", () => {
-  const input = `\
-    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\
-  `;
   assertEquals(part1(input), 8);
 });
 
 Deno.test("part-2", () => {
-  const input = `\
-    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green\
-  `;
   assertEquals(part2(input), 2286);
 });
